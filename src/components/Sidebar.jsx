d8eea9be5fb94da1b078d378ff091149b3b192d4
kaekkr <94045551+kaekkr@users.noreply.github.com>
2022-10-05 13:30:04 +0600

    Final

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,8 +27,8 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#1b1624]">
+        {/* <img src={logo} alt="logo" className="w-full h-14 object-contain"/> */}
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
@@ -38,7 +38,7 @@ const Sidebar = () => {
           <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)}/>
         )}
       </div>
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#583d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
         <NavLinks handleClick={() => setMobileMenuOpen(false)}/>
       </div>
